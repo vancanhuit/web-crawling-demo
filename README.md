e53c5caf7fe6f9b37ce301a0d4d5872c2982d5c3
vancanhuit <vancanhuit@gmail.com>
2018-03-09 19:13:18 +0700

    Fix duplicate feeds

--- README.md
+++ README.md
@@ -3,6 +3,12 @@
 Repository: [https://github.com/vancanhuit/web-crawling-demo](https://github.com/vancanhuit/web-crawling-demo)
 
 - Download and install [Python 3.5+](https://www.python.org/).
-- Install BeautifulSoup 4 by following instructions [here](https://www.crummy.com/software/BeautifulSoup/bs4/doc/).
-- Run `python main.py` and see csv data in `data/` folder.
-- Website used for this demo is [VnExpress](https://vnexpress.net/)
+- Install BeautifulSoup 4 by following instructions [here](https://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-beautiful-soup).
+
+This demo will fetch daily feeds from [VnExpress](https://vnexpress.net/). Run following command:
+
+```sh
+$ python main.py
+```
+
+then go to `data/` directory and open CSV file by Excel.
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import os
 import csv
 import re
+from datetime import datetime
 
 ''' Demo web crawling with BeautifulSoup 4 '''
 
@@ -11,41 +12,52 @@ def not_relative_uri(href):
     return re.compile('^https://').search(href) is not None
 
 
-def write_to_csv_file(feeds, filename, mode='w', write_header=False):
-    with open(filename, mode) as csv_file:
+def add_feeds(feeds):
+    for feed in feeds:
+        # Do some filters before adding
+        if feed.has_attr('target'):
+            continue
+
+        title = feed.get('title')
+        link = feed.get('href')
+        if title is None or link is None:
+            continue
+
+        total_feeds.add((title, link))
+
+
+def write_to_csv_file(filename):
+    with open(filename, 'w') as csv_file:
         writer = csv.writer(csv_file)
+        writer.writerow(['Title', 'Link'])
 
-        if write_header:
-            writer.writerow(['Title', 'Link'])
+        for feed in total_feeds:
+            writer.writerow(feed)
 
-        for feed in feeds:
-            if feed.has_attr('target'):
-                continue
-
-            title = feed.get('title')
-            link = feed.get('href')
-
-            if title is None or link is None:
-                continue
-
-            writer.writerow([title, link])
+        print('All feeds were stored in file {}'.format(filename))
 
 url = 'https://vnexpress.net/'
 page = urllib.request.urlopen(url)
 soup = BeautifulSoup(page, 'html.parser')
 
-data_file = os.path.join(os.getcwd(), 'data', 'vnexpress_feeds.csv')
+total_feeds = set()
+
+today = datetime.date(datetime.now()).isoformat()
+data_file = os.path.join(
+    os.getcwd(), 'data', 'vnexpress_feeds_' + today + '.csv')
 
 new_feeds = soup.find(
     'section', class_='featured container clearfix').find_all(
         'a', class_='', href=not_relative_uri)
 
-sidebar_home_1 = soup.find('section', class_='sidebar_home_1').find_all(
+sidebar_home_1_feeds = soup.find('section', class_='sidebar_home_1').find_all(
     'a', class_='', href=not_relative_uri)
 
-sidebar_home_2 = soup.find('section', class_='sidebar_home_2').find_all(
+sidebar_home_2_feeds = soup.find('section', class_='sidebar_home_2').find_all(
     'a', class_='', href=not_relative_uri)
 
-write_to_csv_file(new_feeds, data_file, write_header=True)
-write_to_csv_file(sidebar_home_1, data_file, mode='a')
-write_to_csv_file(sidebar_home_2, data_file, mode='a')
+add_feeds(new_feeds)
+add_feeds(sidebar_home_1_feeds)
+add_feeds(sidebar_home_2_feeds)
+
+write_to_csv_file(data_file)
