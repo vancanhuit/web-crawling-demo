0382cd895ad23f4f490178c0f2fb0cf08dcfa7c8
vancanhuit <vancanhuit@gmail.com>
2018-03-09 13:56:24 +0700

    Update

--- main.py
+++ main.py
@@ -11,17 +11,23 @@ def not_relative_uri(href):
     return re.compile('^https://').search(href) is not None
 
 
-def write_to_csv_file(filename, feeds):
-    with open(filename, 'w') as csv_file:
+def write_to_csv_file(feeds, filename, mode='w', write_header=False):
+    with open(filename, mode) as csv_file:
         writer = csv.writer(csv_file)
-        writer.writerow(['Title', 'Link'])
+
+        if write_header:
+            writer.writerow(['Title', 'Link'])
 
         for feed in feeds:
+            if feed.has_attr('target'):
+                continue
+
             title = feed.get('title')
             link = feed.get('href')
 
             if title is None or link is None:
                 continue
+
             writer.writerow([title, link])
 
 url = 'https://vnexpress.net/'
@@ -34,9 +40,12 @@ new_feeds = soup.find(
     'section', class_='featured container clearfix').find_all(
         'a', class_='', href=not_relative_uri)
 
-sidebar_home_1 = soup.find(
-    'section', class_='sidebar_home_1').find_all(
-        'a', class_='', href=not_relative_uri)
+sidebar_home_1 = soup.find('section', class_='sidebar_home_1').find_all(
+    'a', class_='', href=not_relative_uri)
 
-write_to_csv_file(data_file, new_feeds)
-write_to_csv_file(data_file, sidebar_home_1)
+sidebar_home_2 = soup.find('section', class_='sidebar_home_2').find_all(
+    'a', class_='', href=not_relative_uri)
+
+write_to_csv_file(new_feeds, data_file, write_header=True)
+write_to_csv_file(sidebar_home_1, data_file, mode='a')
+write_to_csv_file(sidebar_home_2, data_file, mode='a')
