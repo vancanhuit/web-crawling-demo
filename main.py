b20a63de842c6b136a5e96504f0e8ba78254863c
vancanhuit <vancanhuit@gmail.com>
2018-03-09 13:13:01 +0700

    Update

--- main.py
+++ main.py
@@ -2,31 +2,45 @@ import urllib.request
 from bs4 import BeautifulSoup
 import os
 import csv
+import re
 
 ''' Demo web crawling with BeautifulSoup 4 '''
+
+
+def not_relative_uri(href):
+    return re.compile('^https://').search(href) is not None
+
+
+def write_to_csv_file(filename, feeds):
+    with open(filename, 'w') as csv_file:
+        writer = csv.writer(csv_file)
+
+        for feed in feeds:
+            title = feed.get('title')
+            link = feed.get('href')
+
+            if title is None or link is None:
+                continue
+            writer.writerow([title, link])
+
 url = 'https://vnexpress.net/'
 page = urllib.request.urlopen(url)
 soup = BeautifulSoup(page, 'html.parser')
 
-feeds = []
-
-# Get first feed of the day
-new_feeds = soup.find('section', class_="featured container clearfix")
-first_feed = new_feeds.find('a')
-feeds.append((first_feed.get('title'), first_feed.get('href')))
-
-# Get sub feeds beside first feed of the day
-sub_feeds = new_feeds.find('div', class_='sub_featured').find(
-    'ul', attrs={'id': 'list_sub_featured'}).find_all('a', class_="")
-for feed in sub_feeds:
-    feeds.append((feed.get('title'), feed.get('href')))
-
-
-# Store into csv file
 data_file = os.path.join(os.getcwd(), 'data', 'vnexpress_feeds.csv')
-with open(data_file, 'w') as csv_file:
-    writer = csv.writer(csv_file)
-    writer.writerow(['Title', 'Link'])
 
-    for f in feeds:
-        writer.writerow(f)
+new_feeds = soup.find(
+    'section', class_='featured container clearfix').find_all(
+        'a', class_='', href=not_relative_uri)
+
+sidebar_home_1 = soup.find(
+    'section', class_='sidebar_home_1').find_all(
+        'a', class_='', href=not_relative_uri)
+
+sidebar_home_2 = soup.find(
+    'section', class_='sidebar_home_2').find_all(
+        'a', class_='', href=not_relative_uri)
+
+write_to_csv_file(data_file, new_feeds)
+write_to_csv_file(data_file, sidebar_home_1)
+write_to_csv_file(data_file, sidebar_home_2)
